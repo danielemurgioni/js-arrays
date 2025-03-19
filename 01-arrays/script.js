a3139ba11650c083e223ea9365d85f4db6ba7d12
danielemurgioni <dashomew@gmail.com>
2025-03-19 15:52:03 +0100

    [DONE] 01-arrays; eighth exercise

--- 01-arrays/script.js
+++ 01-arrays/script.js
@@ -26,18 +26,19 @@ const firstTeacher = (teachers.shift());
 console.log(firstTeacher);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
-teachers.push('Vanessa')
+teachers.push('Vanessa');
 console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
-teachers.unshift('Sarah')
+teachers.unshift('Sarah');
 console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
 const lewisIndex = (teachers.length -2);
-console.log(lewisIndex)
+console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = (teachers.splice());
+console.log(isTeachersEmpty);
